eadc3d7af7a5af98bc47b57eeb427222428553c3
shairatorio <shairatorio08@gmail.com>
2023-10-24 23:44:33 +0800

    Add widescreen and tallscreen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./dist/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 1/2)' },
+      }
+    },
   },
   plugins: [],
 }
